f848dcf8c5567bc3f70e51b48869e77d5e313940
Sergey Petrosyan <ipetriez@icloud.com>
2024-02-25 19:04:31 +0400

    Create Expenses class and instantiate it inside the main ContentView.

---
+++ iExpense/Expenses.swift
@@ -0,0 +1,13 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Sergey Petrosyan on 25.02.24.
+//
+
+import Foundation
+
+@Observable
+final class Expenses {
+    var items = [ExpenseItem]()
+}
