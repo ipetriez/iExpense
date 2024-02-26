9b65796e8b10f0e70eefbbe211062552e9fc0dd3
Sergey Petrosyan <ipetriez@icloud.com>
2024-02-26 11:31:07 +0400

    Create AddExpenseView.

---
+++ iExpense/AddExpenseView.swift
@@ -0,0 +1,36 @@
+//
+//  AddExpenseView.swift
+//  iExpense
+//
+//  Created by Sergey Petrosyan on 26.02.24.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) { type in
+                        Text(type)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
