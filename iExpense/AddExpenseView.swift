8a12f87aa7368bf71e66f8ffd5dfcf7525018ccc
Sergey Petrosyan <ipetriez@icloud.com>
2025-11-19 20:30:41 +0400

    Use navigation title for user name input inside AddExpenseView.

--- iExpense/AddExpenseView.swift
+++ iExpense/AddExpenseView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AddExpenseView: View {
     @Environment(\.dismiss) var dismiss
-    @State private var name = ""
+    @State private var name = "Add new expense"
     @State private var type = "Personal"
     @State private var amount = 0.0
     let types: [ExpenseType] = [.business, .personal]
@@ -18,7 +18,6 @@ struct AddExpenseView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
                 Picker("Type", selection: $type) {
                     ForEach(types, id: \.self) { type in
                         Text(type.title)
@@ -28,7 +27,7 @@ struct AddExpenseView: View {
                 TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add new expense")
+            .navigationTitle($name)
             .toolbar {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
